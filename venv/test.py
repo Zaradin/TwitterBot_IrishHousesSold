eabee23204ad6c9787100586fadb3da75a76de39
Zaradin <crottyjj@hotmail.com>
2023-01-25 01:42:43 +0000

    Updated Project & Added yaml to twitter auth class.

---
+++ venv/test.py
@@ -0,0 +1,16 @@
+print("""Address Information
+27 CRUACHAN 
+ABBEYSIDE 
+DUNGARVAN 
+ 
+Waterford 
+X35NY58 
+Sale Information
+Date of Sale:	18/08/2022
+Price:	€298,000.00
+Not Full Market Price:	No
+VAT Exclusive:	No
+Description of Property:	Second-Hand Dwelling house /Apartment
+Property Size Desc.:	
+Other Properties in this Sale
+No other properties""")
